9f078eb29f194d64063be562c8e5e4276c66705d
Andrew <red.kasid@gmail.com>
2018-06-21 12:39:46 +0000

    blah

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@ const path = require("path");
 const router = require("express").Router();
 const batchRoutes = require('./batchRoutes');
 const lotRoutes =  require('./lotRoutes');
-
-router.use("/batch", batchRoutes);
-router.use('/lots', lotRoutes)
-
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname,"../client/build/index.html" ));
-});
+const fs = require('fs')
+let ip;
+//router.use('*', (req, res) =>{
+//	let ip = req.headers['x-forwarded-for']
+//	console.log(ip)
+//	fs.appendFile('ip_log.txt','ip : ' +  ip + '\n', ()=>{})
+//})
+//if (ip == '100.8.10.190'){
+	router.use("/batch", batchRoutes);
+	router.use('/lots', lotRoutes)
+	//router.use(function(req, res) {
+	//res.sendFile(path.join(__dirname,"../client/build/index.html" ));
+	//});
+//}
 
 module.exports = router;
